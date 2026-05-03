42bc175656cfe9368c231eb1502ed3753f104103
Darvin Yi <yidarvin@gmail.com>
2026-05-03 00:40:08 -0700

    fix: use @prisma/client import to respect engineType library setting

--- server/src/lib/prisma.ts
+++ server/src/lib/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '../../node_modules/.prisma/client';
+import { PrismaClient } from '@prisma/client';
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
